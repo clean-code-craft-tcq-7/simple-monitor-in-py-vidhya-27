729d261c2c356c1e8e6b5aeaba70a640eefde66e
vidhya-27 <vidhyaraja049@gmail.com>
2025-07-29 10:55:54 +0530

    changes made on July 29

--- monitor.test.py
+++ monitor.test.py
@@ -3,9 +3,15 @@ from monitor import vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+    def vital_are_ok(self):
+      self.assertTrue(vitals_ok(98, 72, 99))
+      self.assertTrue(vitals_ok(102, 100, 90)) #boundary conditions test
+      self.assertTrue(vitals_ok(95, 60, 90)) #boundary conditions test
+    def vital_not_ok(self):
+       self.assertFalse(vitals_ok(105, 120, 80))
+       self.assertFalse(vitals_ok(103, 101, 89)) #boundary conditions test
+       self.assertFalse(vitals_ok(94, 59, 89))
+       
 
 
 if __name__ == '__main__':
